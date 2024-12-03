c54b1b0015815f99b03ca7713dd66db131008674
virgil <virgil.dinu@etu.u-paris.fr>
2024-12-03 16:13:34 +0100

    Update Ressource.java
    ajout de nouveau constructeur pour gerer le fichier txt.

--- Ressource.java
+++ Ressource.java
@@ -1,4 +1,5 @@
 
+
 package projet_paa;
 
 
@@ -7,11 +8,18 @@ public class Ressource {
     Classe permettant de representer des instances de Ressources qui devront etre partagées entre les Colons
     */
     private int NumeroRessource; //Defini un numero pour la ressource instanciée
+    private String nomRessource;
 
 
     public Ressource(int i) // Constructeur de la ressource
     {
         this.NumeroRessource = i;
+    }
+    
+    public Ressource(int i, String n)
+    {
+        this.NumeroRessource = i;
+        this.nomRessource = n;
     }
 
     public int getNumeroRessource() //fonction pour recuperer le numero de la ressource instanciée
@@ -22,7 +30,18 @@ public class Ressource {
     public void setNumeroRessource(int num){
         this.NumeroRessource = num;
     }
-
+    
+    public String getNomRessource()
+    {
+        return nomRessource;
+    }
+    
+    public void setNomRessource(String n)
+    {
+        nomRessource = n;
+    }
+    
+    @Override
     public String toString() { //affiche le numéro de la ressource
         return "Ressource " + NumeroRessource;
     }
